dffcc574a70896a183f4c3b998d8c16870976694
Khoi Nguyen <khoi.nguyen@kal.com>
2025-07-29 16:02:24 +0700

    Added lazygit for neovim

--- lua/zac/plugins/treesitter.lua
+++ lua/zac/plugins/treesitter.lua
@@ -1,15 +1,30 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-            configs.setup({
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "c_sharp", "typescript", "tsx", "json", "css"},
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end
-    }
+			configs.setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"elixir",
+					"heex",
+					"javascript",
+					"html",
+					"c_sharp",
+					"typescript",
+					"tsx",
+					"json",
+					"css",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
--- lua/zac/remap.lua
+++ lua/zac/remap.lua
@@ -67,3 +67,5 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+
+vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
