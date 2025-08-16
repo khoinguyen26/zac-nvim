49ad034e37988734ced807333e17531cdcd3fdd9
khoinguyen26 <khoinguyen261101@gmail.com>
2025-08-16 23:06:41 +0700

    update themes config

---
+++ lua/zac/plugins/themes.lua
@@ -0,0 +1,49 @@
+local function get_os()
+    if package.config:sub(1, 1) == '\\' then
+        return "Windows"
+    else
+        return "Linux"
+    end
+end
+
+
+local my_os = get_os()
+
+return {
+    {
+        "rose-pine/neovim",
+        lazy = false,
+        name = "rose-pine",
+        -- enabled = (my_os == "Linux"),
+        enabled = false,
+        priority = 1000,
+        config = function()
+            -- load the colorscheme here
+            vim.cmd([[colorscheme rose-pine]])
+        end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        enabled = false,
+        config = function()
+            -- load the colorscheme here
+            vim.cmd([[colorscheme tokyonight-night]])
+        end,
+    },
+    {
+        'sainnhe/everforest',
+        lazy = false,
+        priority = 1000,
+        enabled = true,
+        config = function()
+            -- Optionally configure and load the colorscheme
+            -- directly inside the plugin declaration.
+            vim.g.everforest_enable_italic = true
+            vim.cmd "let g:everforest_background = 'hard'"
+            vim.cmd.colorscheme('everforest')
+        end
+    }
+}
--- lua/zac/set.lua
+++ lua/zac/set.lua
@@ -14,25 +14,24 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 
 
-vim.opt.swapfile = false                               -- Disable swap files
-vim.opt.backup = false                                 -- Disable backup files
+vim.opt.swapfile = false -- Disable swap files
+vim.opt.backup = false   -- Disable backup files
 
 -- make it cross-platform
 local home = os.getenv("HOME") or os.getenv("USERPROFILE")
 
 vim.opt.undodir = home .. "/.vim/undodir" -- Set the directory for undo files
-vim.opt.undofile = true                                -- Enable persistent undo
+vim.opt.undofile = true                   -- Enable persistent undo
 
-vim.opt.hlsearch = false                               -- Disable highlighting of search matches
-vim.opt.incsearch = true                               -- Enable incremental search
+vim.opt.hlsearch = false                  -- Disable highlighting of search matches
+vim.opt.incsearch = true                  -- Enable incremental search
 
-vim.opt.termguicolors = true                          -- Enable 24-bit RGB color in the terminal
+vim.opt.termguicolors = true              -- Enable 24-bit RGB color in the terminal
 
-vim.opt.scrolloff = 8                                  -- Keep 8 lines visible above and below the cursor
-vim.opt.signcolumn = "yes"                             -- Always show the sign column (for diagnostics, etc.)
-vim.opt.isfname:append("@-@")                          -- Include '@' and '-' in filenames
+vim.opt.scrolloff = 8                     -- Keep 8 lines visible above and below the cursor
+vim.opt.signcolumn = "yes"                -- Always show the sign column (for diagnostics, etc.)
+vim.opt.isfname:append("@-@")             -- Include '@' and '-' in filenames
 
-vim.opt.updatetime = 50                                -- Time in milliseconds for triggering events (like CursorHold)
+vim.opt.updatetime = 50                   -- Time in milliseconds for triggering events (like CursorHold)
 
-vim.opt.colorcolumn = "80"                             -- Highlight column 80 (typically for line length limits)
-
+vim.opt.colorcolumn = "80"                -- Highlight column 80 (typically for line length limits)
