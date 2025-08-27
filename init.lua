831845a07fb3ab15e97de0f18c6f5db258d924cc
khoinguyen26 <khoinguyen261101@gmail.com>
2025-08-27 03:53:37 +0000

    sync

--- init.lua
+++ init.lua
@@ -1,15 +1,1 @@
-require("zac.remap")
 require("zac.lazy")
-require("zac.set")
-require("zac.lsp")
-
--- Set up my and load my "lua/zac/plugins" folder
-require("lazy").setup({
-    spec = { import = "zac/plugins" },
-    -- Configure any other settings here. See the documentation for more details.
-    -- automatically check for plugin updates
-    checker = { enabled = true },
-    change_detection = {
-        notify = false
-    }
-})
--- lua/zac/plugins/nvim-lspconfig.lua
+++ lua/zac/plugins/nvim-lspconfig.lua
@@ -1,18 +1,37 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		local map = vim.keymap.set
-		local opts = { noremap = true, silent = true }
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "mason-org/mason.nvim",
+            "mason-org/mason-lspconfig.nvim",
+        },
+        config = function()
+            local map = vim.keymap.set
+            local opts = { noremap = true, silent = true }
 
-		map("n", "gd", vim.lsp.buf.definition, opts)
-		map("n", "gi", vim.lsp.buf.implementation, opts)
-		map("n", "gr", vim.lsp.buf.references, opts)
-		map("n", "gy", vim.lsp.buf.type_definition, opts)
-		map("n", "K", vim.lsp.buf.hover, opts)
-		map("n", "<leader>rn", vim.lsp.buf.rename, opts)
-		map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-		map("i", "<C-l>", function()
-			require("blink.cmp").show()
-		end, { desc = "Trigger Blink completion menu" })
-	end,
+            map("n", "gd", vim.lsp.buf.definition, opts)
+            map("n", "gD", vim.lsp.buf.declaration, opts)
+            map("n", "gi", vim.lsp.buf.implementation, opts)
+            map("n", "gr", vim.lsp.buf.references, opts)
+            map("n", "gy", vim.lsp.buf.type_definition, opts)
+            map("n", "gs", vim.lsp.buf.signature_help, opts)
+            map("n", "K", vim.lsp.buf.hover, opts)
+            map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+            map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+            map("i", "<C-l>", function()
+                require("blink.cmp").show()
+            end, { desc = "Trigger Blink completion menu" })
+
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                handlers = {
+                    function(server_name)
+                        require("lspconfig")[server_name].setup({})
+                    end
+
+                }
+            })
+        end,
+
+    }
 }
