895d3a4765c39ff5c3c952b664740b3defa0f798
khoinguyen26 <khoinguyen261101@gmail.com>
2025-09-20 02:02:23 +0700

    sync

--- lua/zac/plugins/conform-nvim.lua
+++ lua/zac/plugins/conform-nvim.lua
@@ -1,28 +1,28 @@
 return {
-	"stevearc/conform.nvim",
-	dependencies = { "mason.nvim" },
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-			typescript = { "prettierd", "prettier", stop_after_first = true },
-			javascriptreact = { "prettier" },
-			typescriptreact = { "prettier" },
-			json = { "prettier" },
-			markdown = { "prettier" },
-		},
-		formatters = {
-			prettier = {
-				args = { "--trailing-comma", "es5" },
-			},
-			prettierd = {
-				args = { "--trailing-comma", "es5" },
-			},
-		},
-		-- Set up format on save
-		format_on_save = {
-			timeout_ms = 1000,
-			lsp_fallback = true,
-		},
-	},
+    "stevearc/conform.nvim",
+    dependencies = { "mason.nvim" },
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            javascript = { "prettier", stop_after_first = true },
+            typescript = { "prettier", stop_after_first = true },
+            javascriptreact = { "prettier" },
+            typescriptreact = { "prettier" },
+            json = { "prettier" },
+            markdown = { "prettier" },
+        },
+        formatters = {
+            prettier = {
+                args = { "--trailing-comma", "es5" },
+            },
+            prettierd = {
+                args = { "--trailing-comma", "es5" },
+            },
+        },
+        -- Set up format on save
+        format_on_save = {
+            timeout_ms = 1000,
+            lsp_fallback = true,
+        },
+    },
 }
--- lua/zac/plugins/nvim-lspconfig.lua
+++ lua/zac/plugins/nvim-lspconfig.lua
@@ -1,54 +1,56 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"mason-org/mason.nvim",
-			"mason-org/mason-lspconfig.nvim",
-		},
-		config = function()
-			local map = vim.keymap.set
-			local opts = { noremap = true, silent = true }
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "mason-org/mason.nvim",
+            "mason-org/mason-lspconfig.nvim",
+        },
+        config = function()
+            local map = vim.keymap.set
+            local opts = { noremap = true, silent = true, desc = "[g]o to [d]efinition" }
 
-			map("n", "gd", vim.lsp.buf.definition, opts)
-			map("n", "gD", vim.lsp.buf.declaration, opts)
-			map("n", "gi", vim.lsp.buf.implementation, opts)
-			map("n", "gr", vim.lsp.buf.references, opts)
-			map("n", "gy", vim.lsp.buf.type_definition, opts)
-			map("n", "gs", vim.lsp.buf.signature_help, opts)
-			map("n", "K", vim.lsp.buf.hover, opts)
-			map("n", "<leader>rn", vim.lsp.buf.rename, opts)
-			map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-			map("i", "<C-l>", function()
-				require("blink.cmp").show()
-			end, { desc = "Trigger Blink completion menu" })
+            map("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true, desc = "[g]o to [d]efinition" })
+            map("n", "gD", vim.lsp.buf.declaration, { noremap = true, silent = true, desc = "[g]o to [D]eclaration" })
+            map("n", "gi", vim.lsp.buf.implementation,
+                { noremap = true, silent = true, desc = "[g]o to [i]mplementation" })
+            map("n", "gr", vim.lsp.buf.references, { noremap = true, silent = true, desc = "[g]o to [r]eferences" })
+            map("n", "gy", vim.lsp.buf.type_definition,
+                { noremap = true, silent = true, desc = "[g]o to t[y]pe definition" })
+            map("n", "gs", vim.lsp.buf.signature_help, { noremap = true, silent = true, desc = "[g]o [s]ignature help" })
+            map("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true, desc = "Show info" })
+            map("n", "<leader>rn", vim.lsp.buf.rename, { noremap = true, silent = true, desc = "rename" })
+            map("n", "<leader>ca", vim.lsp.buf.code_action, { noremap = true, silent = true, desc = "[c]ode [a]ction" })
+            map("i", "<C-l>", function()
+                require("blink.cmp").show()
+            end, { desc = "Trigger Blink completion menu" })
 
-			require("mason").setup({
-				registries = {
-					"github:mason-org/mason-registry",
-					"github:Crashdummyy/mason-registry",
-				},
-			})
-			require("mason-lspconfig").setup({
-				handlers = {
-					function(server_name)
-						require("lspconfig")[server_name].setup({})
-					end,
-				},
+            require("mason").setup({
+                registries = {
+                    "github:mason-org/mason-registry",
+                    "github:Crashdummyy/mason-registry",
+                },
+            })
+            require("mason-lspconfig").setup({
+                handlers = {
+                    function(server_name)
+                        require("lspconfig")[server_name].setup({})
+                    end,
+                },
 
-				["vtsls"] = function()
-					require("lspconfig").vtsls.setup({})
+                ["vtsls"] = function()
+                    require("lspconfig").vtsls.setup({})
 
-					-- Tell nvim-lspconfig to explicitly disable ts_ls for all TypeScript filetypes
-					require("lspconfig").ts_ls.disable_by_filetype = {
-						"javascript",
-						"javascriptreact",
-						"javascript.jsx",
-						"typescript",
-						"typescriptreact",
-						"typescript.tsx",
-					}
-				end,
-			})
-		end,
-	},
+                    -- Tell nvim-lspconfig to explicitly disable ts_ls for all TypeScript filetypes
+                    require("lspconfig").ts_ls.disable_by_filetype = {
+                        "javascript",
+                        "javascriptreact",
+                        "javascript.jsx",
+                        "typescript",
+                        "typescriptreact",
+                        "typescript.tsx",
+                    }
+                end,
+            })
+        end,
+    },
 }
